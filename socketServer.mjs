23435208d064c4037d4bc960e20cb8c7aad27883
Stephen Smith <wearypossum4770@yahoo.com>
2021-08-28 12:48:36 -0500

    sync

--- socketServer.mjs
+++ socketServer.mjs
@@ -1,43 +1,10 @@
-import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { createServer } from "http";
-import { Server } from "socket.io";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const app = express();
+import { WebSocketServer } from 'ws';
 const PORT = 7625;
-const options = {
-  cors: {
-    origin: [`http://localhost:${7625}`],
-    methods: ["GET", "POST"]
-  }};
-const httpServer = createServer(app);
-const io = new Server(httpServer, options);
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/src/assets/chat_index.html`);
-});
-io.emit("some event", {
-  someProperty: "some value",
-  otherProperty: "other value",
-}); // This will emit the event to all connected sockets
+const wss = new WebSocketServer({ port: PORT });
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log(`received: ${message}`);
+  });
 
-io.on("connection", socket => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-  socket.on("chat message", msg => {
-    io.emit("chat message", msg);
-    console.log(`message: ${msg}`);
-  });
-});
-httpServer.listen(PORT, () => {
-  console.log(`WEBSOCKET listening on port: ${PORT}`);
-});
-// consi  der using http2 server http2
-// serverOptions = {  allowHTTP1: true}
-// serverOptions{
-//   key: fs.readFileSync("/tmp/key.pem"),
-//   cert: fs.readFileSync("/tmp/cert.pem")
-// }
+  ws.send('something');
+})
