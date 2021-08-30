2f8a399071e14bd16272c1bc1c6304e9f12dd467
stephen smith ecs <wearypossum4770@yahoo.com>
2021-08-30 17:14:57 -0400

    sync

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import router from "./router";
 import Vue from "vue";
 import i18n from "./i18n";
 import store from "./store";
-import socket from "./plugins/websocket";
 import "./plugins/axios";
 import "./assets/css/w3_version_4.css";
 // import { createProvider } from "./vue-apollo";
@@ -16,7 +15,6 @@ new Vue({
   router,
   i18n,
   store,
-  socket,
   // apolloProvider: createProvider(),
   render: h => h(App),
 }).$mount("#app");
--- src/plugins/websocket.js
+++ src/plugins/websocket.js
@@ -3,7 +3,7 @@ const localIP = "localhost";
 const networkIP = "0.0.0.0";
 const port = 7625;
 const networkConnection = false;
-
+// https://a2i2.deakin.edu.au/2020/04/23/real-time-data-transfer-using-vue-and-socket-io-part-1-of-3/
 const connectSocket = () => {
   const url = networkConnection
     ? `http://${networkIP}:${port}`
