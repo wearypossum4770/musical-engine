493ac035227399792280f6a913b941ccf1b7fa66
Stephen Smith <wearypossum4770@yahoo.com>
2021-09-05 18:46:52 -0400

    sync

--- server.mjs
+++ server.mjs
@@ -32,6 +32,16 @@ app.use(
   }),
 );
 // app.use('/vue_socket_test')
+app.use((req, res, next)=>{
+  var err = req?.session?.error;
+  var msg = req?.session?.success;
+  delete req?.session?.error;
+  delete req?.session?.success;
+  res.locals.message = '';
+  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+  next();
+});
 app.post("/uploads", async (req, res) => {
   // const readabale = createReadStream(fileName);
   // const writeable = createWriteStream(
